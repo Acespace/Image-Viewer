acb0bbb6467dcaa708bcd0f69313da949c414b97
Acespace <alston.clark17@gmail.com>
2013-12-08 20:56:26 -0500

    Adding picture filters
    Here are two of my filters: 
    
    A "tie-dye" filter and a "black and white filter"

--- filters.py
+++ filters.py
@@ -1,1 +1,51 @@
 #Here
+
+def tieDye(new_image):
+
+      for x in range (len(new_image)):
+
+          for y in range (len(new_image)):
+
+              h,i,j = new_image[x][y][0],new_image[x][y][1],new_image[x][y][2]
+      
+              a,b,c = new_image[x][y][0],new_image[x][y][1],new_image[x][y][2]
+              average =(a + b + c) / 3
+
+              if h > average:
+                h = 255                
+              elif h < average:
+                h = average
+              if i > average:
+                i = 255                
+              elif i < average:
+                i = average                
+              if j > average:
+                j = 255                
+              elif j < average:
+                j = average     
+      index =((h),(i),(j))      
+      new_image[x][y] = index
+      
+  return new_image
+  
+  def BlackandWhite(refImage):
+    
+  for x in range (len(refImage)):
+
+    for y in range (len(refImage)):
+
+        index = refImage[x][y]
+        a,b,c = refImage[x][y][0],refImage[x][y][1],refImage[x][y][2]
+
+        average =(a + b + c) / 3
+
+        index =(average,average,average)
+
+        refImage[x][y] = index
+    return refImage
+
+
+  
+  
+  
+  

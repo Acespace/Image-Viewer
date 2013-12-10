fd8ebb68c577a9986a602bdd915e2884c5e3427a
vforeman <victor.foreman@bison.howard.edu>
2013-12-09 19:53:33 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,50 +1,85 @@
 from pygame import locals
 import pygame as view
-from PIL import ImageGrab
+import ImageGrab as cam
+from pygame import time
+from pygame import image
+import filter_functions as fil
 BLACK=(0,0,0)
 WHITE=(255,255,255)
+
 OLIVE=(128,128,0)
 MAROON=(128,0,0)
 BACKGROUND=(800,800)
 CANVAS=(600,600)
 
+#grab screenshot backgound
+cam.grab((0,0,800,800)).save('screenst.png','png')
+
+filters=[fil.filterone(),fil.filtertwo(),fil.filterthree()]
+
+
 
 def main():
     view.init()
     FPS=30
-    clocky=view.time.Clock
+    #canvas
+    canvas=view.Surface((500,600))
+    canvasrect=canvas.get_rect()
+    canvas.fill((250,250,250))
+    
     #screen
-    screen=view.display.set_mode((800,800),locals.NOFRAME,32)
+    screen=view.display.set_mode((600,600),locals.NOFRAME,32)
     view.display.set_caption("imageviewer")
     screen.fill((OLIVE))
+    canvas=canvas.convert_alpha()
+    canvas.set_alpha(127)
     #background
-    background=view.Surface((800,800))
-    background.fill((250,250,250))
-    #buttons    
+    background=image.load('screenst.png')
+    #background=view.Surface((800,800))
+    #background.fill((250,250,250))
+    #buttons 
+    background.blit(canvas,canvasrect)
     b1=view.Surface((30,30))
+    b1.fill(OLIVE)
     br1=b1.get_rect()
     br1.center=(85,185)
     background.blit(b1,br1)
     b2=view.Surface((30,30))
+    b2.fill(OLIVE)
     br2=b2.get_rect()
     br2.center=(135,285)
     background.blit(b2,br2)
     b3=view.Surface((30,30))
+    b3.fill(OLIVE)
     br3=b3.get_rect()
     br3.center=(365,485)
     background.blit(b3,br3)
     b4=view.Surface((30,30))
+    b4.fill(OLIVE)
     br4=b4.get_rect()
     br4.center=(485,485)
     background.blit(b4,br4)
     buttons=[b1,b2,b3,b4]
     buttonrect=[br1,br2,br3,br4]
     screen.blit(background,(0,0))
-    while True:    
+
+
+
+
+
+    while True: 
+        #next frame   
         view.display.update()
-        #clocky.tick(FPS)
-        for event in view.event.get():
-            if event.type==locals.QUIT:
+        for ev in view.event.get():
+            #mouse event
+            if ev.type==locals.MOUSEBUTTONDOWN:
+                mousepos=view.mouse.get_pos()
+                for button in [0,1,2,3]:                
+                    if buttonrect[button].collidepoint(mousepos):
+                       view.quit()
+                        #buttonrect[button]=buttonrect[button]
+                        #filters[button]()
+            if ev.type==locals.QUIT:
                 pass
                 #view.quit()
 
